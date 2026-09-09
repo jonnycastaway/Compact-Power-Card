88ad1aa728dc6873b914db6c5a1e40b98d156b32
dieloebls <dieloebls@users.noreply.github.com>
2026-09-09 10:55:03 +0200

    feat: PV label - larger gap between icon and power value (margin-top -6px -> 6px, like devices)

--- compact-power-card.js
+++ compact-power-card.js
@@ -4277,7 +4277,7 @@ class CompactPowerCard extends CompactPowerCardBase {
               (lbl) => html`<div class="overlay-item pv-label-marker" data-index="${pvLabelItems.indexOf(lbl)}" style="left:${lbl.leftPct}%; top:${lbl.topPct}%;">
                 <div class="aux-marker pv-label-marker clickable" @click=${() => this._openMoreInfo(lbl.entity || null)}>
                 <ha-icon icon="${lbl.icon}" style="gap: 0px; color:${lbl.color}; opacity:1; --mdc-icon-size: calc(18px * var(--cpc-scale, 1)); filter:${allowGlow && lbl.numeric !== 0 ? `drop-shadow(0 0 8px ${lbl.color})` : "none"};"></ha-icon>
-                  <div class="aux-label" style="margin-top: -6px; padding-bottom: 0px; color:${lbl.color}; opacity:${lbl.hidden ? 0.35 : lbl.opacity};">${renderValue(lbl.val)}</div>
+                  <div class="aux-label" style="margin-top: 6px; padding-bottom: 0px; color:${lbl.color}; opacity:${lbl.hidden ? 0.35 : lbl.opacity};">${renderValue(lbl.val)}</div>
                   ${lbl.secondary_entity && this._hass?.states?.[lbl.secondary_entity] && (() => {
                     return html`<div class="aux-sub-label label-secondary" style="color:${lbl.color}; opacity:${lbl.hidden ? 0.35 : lbl.opacity}; font-size:calc(10px * var(--cpc-scale, 1) * var(--cpc-text-scale, 1)); ">${this._formatSecondaryOnly(lbl)}</div>`;
                   })()}
